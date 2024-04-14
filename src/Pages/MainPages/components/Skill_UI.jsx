68e477db2453ac3d10f9d7430de77ad6ba1ee4b9
annlin718 <benny800718@gmail.com>
2024-04-14 17:44:32 +0800

    20240414

---
+++ src/Pages/MainPages/components/Skill_UI.jsx
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from "uuid";
+
+const Skill_UI = () => {
+  let SkillType = [
+    [["前端"], ["開發應用"]],
+    [["後端/DB"], ["開發應用"]],
+    [["多媒體"], ["設計製作"]],
+    [["辦公軟體"], ["應用"]],
+  ];
+
+  let SkillList = [
+    [
+      "HTML",
+      "CSS/SCSS",
+      "JavaScript",
+      "jQuery",
+      "GSAP(動畫)",
+      "Webpack",
+      "Git / GitHub",
+      "React / Vue",
+    ],
+    ["Asp.Net (C#)", "SQL Server"],
+    ["Photoshop", "Illustrator ", "Flash/Animate", "Premiere", "Figma"],
+    ["PowerPoint", "Word", "Excel", "Outlook"],
+  ];
+
+  return (
+    <div className="divSkill">
+      {SkillType.map(function (Type, index) {
+        return (
+          <div key={uuidv4()} className="SkillBlock">
+            <div className={"SkillTitle SkillTitle" + index}>
+              {Type[0]} <br /> {Type[1]}
+            </div>
+            <div className="SkillList">
+              {SkillList[index].map(function (list) {
+                return <span key={uuidv4()}>{list}</span>;
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Skill_UI;
---
+++ src/Pages/MainPages/components/Thought.jsx
@@ -0,0 +1,18 @@
+import ThoughtPicURL from "../../../Assets/Thought.png";
+
+const Thought_UI = () => {
+  return (
+    <div className="divThought">
+      <img src={ThoughtPicURL} alt="" />
+      <h2>
+        以人為本的設計思維
+        <br />
+        創造舒適、貼心的產品服務
+        <br />
+        專注細節與合理性
+      </h2>
+    </div>
+  );
+};
+
+export default Thought_UI;
